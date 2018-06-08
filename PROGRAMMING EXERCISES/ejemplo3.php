3d2a0829a87c2b4c25dd02a5b0ffba121795801d
Manuel Perez <35848253+manper-04@users.noreply.github.com>
2018-06-08 12:05:52 -0700

    Add files via upload

---
+++ PROGRAMMING EXERCISES/ejemplo3.php
@@ -0,0 +1,13 @@
+<?php
+
+$a=5;
+$b=6;
+
+echo "La suma de " . $a . " + " . $b. " es : " . "<b>" . ($a+$b). "</b>" . "<hr>" .
+"La multiplicacion de " . $a . " x " . $b . " es : " . "<b>" . ($a * $b) . "</b>" . "<hr>" .
+"La division de " . $a  . " / " . $b. " es : " . "<b>" . ($a /$b) ."</b>" ."<hr>" .
+"La resta de " . $a  . " - " . $b. " es : " . "<b>" . ($a - $b) . "</b>" . "<hr>" ;
+
+
+
+ ?>
---
+++ PROGRAMMING EXERCISES/examen.php
@@ -0,0 +1,11 @@
+<?php
+
+echo "<pre>" . "<center>"."
+xxx           xxx        xxx     xxx      xx    xx      xx   xxxxxxxxxxx   xx
+xx xx       xx xx     xx    xx   xx xx    xx    xx      xx   xx            xx
+xx  xx     xx  xx     xx    xx   xx  xx   xx    xx      xx   xxxxxxxxxxx   xx
+xx   xx   xx   xx     xx xx xx   xx   xx  xx    xx      xx   xxxxxxxxxxx   xx
+xx    xx xx    xx     xx    xx   xx    xx xx    xx      xx   xx            xxxxxxxxxxx
+xx     xxx     xx     xx    xx   xx     xxx       xxxxxx     xxxxxxxxxxx   xxxxxxxxxxx";
+
+ ?>
